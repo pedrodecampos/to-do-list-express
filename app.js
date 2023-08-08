a4bd2120f97ba0edc90faaff8d646674194cdaff
Pedro <pedroafonso939@Yahoo.com.br>
2023-08-08 19:43:47 -0300

    Estrutura em node feita, agora terminar em mongodb

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // consumindo o express
 const express = require("express");
+
+// importando o router & utilizando como um middlewares
+const checklistRouter = require("./src/routes/checklist");
+// instanciando o express
+const app = express();
+
+// inicializando a rota && e todas rotas serao derivada do checklist
+app.use("/checklist", checklistRouter);
+
+// chamando o middlewares
+app.use(express.json()); // para verificar se quando fazer uma chamda web se tem algum json dentro
+
+// start no servidor
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
---
+++ src/routes/checklist.js
@@ -0,0 +1,39 @@
+// arquivo para criar rotas
+const express = require("express");
+const { route } = require("express/lib/application");
+
+//  ferramenta para criar rota nos arquivos
+const router = express.Router();
+
+// criando uma nova rota
+router.get("/", (req, res) => {
+  console.log("Ola");
+  res.send;
+});
+
+// criando uma rota post
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.title);
+});
+
+// criando paramentro para uma rota
+router.get("/:id", (req, res) => {
+  console.log(req.params.body);
+  res.send(`ID: ${req.params.id}`);
+});
+
+// rota do tipo put [ATUALIZAR]
+router.put("/:id", (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.params);
+});
+
+// rota do tipo delete [delete]
+router.delete("/id:", (req, res) => {
+  console.log(req.params.body);
+  res.send(`ID: ${req.params.params}`);
+});
+
+// exportando a rota
+module.exports = router;
